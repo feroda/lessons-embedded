f6e40d156a2bdb5b8a5fc4d5545708ae65daa25f
Luca Ferroni <luca@befair.it>
2016-03-08 01:28:55 +0100

    Pulizia anche in module_hello_with_ops

--- module_hello/.built-in.o.cmd
+++
@@ -1,1 +0,0 @@
-cmd_drivers/char/examples//built-in.o :=  rm -f drivers/char/examples//built-in.o; ar rcsD drivers/char/examples//built-in.o
--- module_hello_with_ops/.hello1.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_drivers/char/examples//hello1.ko := ld -r -m elf_x86_64 -T ./scripts/module-common.lds --build-id  -o drivers/char/examples//hello1.ko drivers/char/examples//hello1.o drivers/char/examples//hello1.mod.o
